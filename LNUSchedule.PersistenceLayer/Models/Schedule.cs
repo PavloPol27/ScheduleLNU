6612ded81aee0c9a946317633e38c493514e2b08
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-04-08 09:20:44 +0300

    Add schedule

---
+++ LNUSchedule.PersistenceLayer/Models/Schedule.cs
@@ -0,0 +1,13 @@
+﻿namespace LNUSchedule.PersistenceLayer.Models
+{
+    public class Schedule
+    {
+        public string Title { get; set; } = string.Empty;
+
+
+        public Student? Student { get; set; }
+
+
+        public List<Event> Events { get; set; } = new List<Event>();
+    }
+}
