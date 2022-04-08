9484b4e757d1636098f8f0033102879187930c0c
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-04-08 09:14:04 +0300

    Add Theme model

---
+++ LNUSchedule.PersistenceLayer/Models/Theme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LNUSchedule.PersistenceLayer.Models
+{
+    public class Theme
+    {
+        [Key]
+        public string Title { get; set; } = string.Empty;
+
+
+        public string ForeColor { get; set; } = string.Empty;
+
+
+        public string BackColor { get; set; } = string.Empty;
+
+
+        public string Font { get; set; } = string.Empty;
+
+        
+        public int FontSize { get; set; } = 14;
+    }
+}
