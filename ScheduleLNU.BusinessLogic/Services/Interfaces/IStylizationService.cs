f0573691c48f29f54137c9a1ab42cad1fe53c4e1
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-04-19 13:00:50 +0300

    Create interface and base service

---
+++ ScheduleLNU.BusinessLogic/Services/Interfaces/IStylizationService.cs
@@ -0,0 +1,13 @@
+﻿using ScheduleLNU.DataAccess.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ScheduleLNU.BusinessLogic.Services.Interfaces
+{
+    public interface IStylizationService
+    {
+        public Task<IEnumerable<EventStyle>> GetAllEventStylesAsync(uint studentId);
+
+        public Task<IEnumerable<Theme>> GetAllThemesAsync(uint studentID);
+    }
+}
---
+++ ScheduleLNU.Presentation/Controllers/SettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ScheduleLNU.Presentation.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly ILogger<SettingsController> logger;
+
+        public SettingsController(ILogger<SettingsController> injectedLogger)
+        {
+            logger = injectedLogger;
+        }
+
+        [HttpGet]
+        public IActionResult Themes()
+        {
+            logger.LogInformation("Student views all themes");
+            return View();
+        }
+    }
+}
