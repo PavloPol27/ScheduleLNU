e74859d2dd7e547af1409786f5fcd9d3e65e2d77
Dmytro Balabukh <demabalab@gmail.com>
2022-04-21 00:50:22 +0300

    draft commit to scaffold delete

--- ScheduleLNU.BusinessLogic/Services/ScheduleService.cs
+++ ScheduleLNU.BusinessLogic/Services/ScheduleService.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using ScheduleLNU.BusinessLogic.DTOs;
 using ScheduleLNU.BusinessLogic.Services.Interfaces;
 using ScheduleLNU.DataAccess.Entities;
@@ -22,6 +24,22 @@ namespace ScheduleLNU.BusinessLogic.Services
             return (await scheduleRepository
                 .SelectAllAsync(x => x.Student.Id == studentId))
                 .Select(x => new ScheduleDto { Id = x.Id, Title = x.Title });
+        }
+
+        public async Task<bool> DeleteScheduleAsync(int studentId, int scheduleId)
+        {
+            try
+            {
+                Schedule schedule = (await scheduleRepository.SelectAllWithIncludeAsync((schedule) =>
+                schedule.Id == scheduleId && schedule.Student.Id == studentId,
+                (entity) => entity.Student)).FirstOrDefault();
+                await scheduleRepository.DeleteAsync(schedule);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
