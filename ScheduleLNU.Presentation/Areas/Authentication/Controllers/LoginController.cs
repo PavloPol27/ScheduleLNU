244fbc64c13e5d5bc93d6b8b85215b407c0f7f11
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-05-02 14:55:57 +0300

    Squashed commit of the following:
    commit f305dccbb5bf0df25a5be0f200240af9cf9834fb
    Author: Dmytro Balabukh <demabalab@gmail.com>
    Date:   Sun May 1 22:23:34 2022 +0300
    
        changed forgot-password to already have an account on register page
    
    commit d39ce5188a6c1bc21741d587cf1eaf9138e1689c
    Author: Dmytro Balabukh <demabalab@gmail.com>
    Date:   Sun May 1 21:55:08 2022 +0300
    
        add implementation

---
+++ ScheduleLNU.Presentation/Areas/Authentication/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScheduleLNU.BusinessLogic.DTOs;
+
+namespace ScheduleLNU.Presentation.Areas.Authentication.Controllers
+{
+    [Area("authentication")]
+    [Route("[area]/login")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            return View();
+        }
+    }
+}
---
+++ ScheduleLNU.Presentation/Areas/Authentication/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using ScheduleLNU.BusinessLogic.DTOs;
+
+namespace ScheduleLNU.Presentation.Areas.Authentication.Controllers
+{
+    [Area("authentication")]
+    [Route("[area]/register")]
+    public class RegisterController : Controller
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult Register(RegisterDto registerDto)
+        {
+            return View();
+        }
+    }
+}
